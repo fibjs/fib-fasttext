f6ddbca8a11eb2fddea1df53a74edb6edf8ad0c2
xicilion <lion@9465.net>
2023-12-13 22:27:50 +0800

    Build system: Simplify build configuration.

--- build.cmake
+++ build.cmake
@@ -1,8 +1,3 @@
 cmake_minimum_required(VERSION 3.0)
 
-get_filename_component(name ${CMAKE_CURRENT_SOURCE_DIR} NAME)
-
-include(fib-addon/build_tools/cmake-scripts/get_env.cmake)
-
-set(WORK_ROOT "${CMAKE_CURRENT_SOURCE_DIR}")
-build("${CMAKE_CURRENT_SOURCE_DIR}" "${WORK_ROOT}" "${name}")
+include(fib-addon/build.cmake)
